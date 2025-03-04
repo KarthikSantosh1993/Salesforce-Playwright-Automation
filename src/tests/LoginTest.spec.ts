e88d75241ed0ab32973fc4ea13e49c8d1cce3c22
karthik <krthiksantosh@gmail.com>
2025-03-04 22:52:10 +0530

    features: Environment Encryption

--- src/tests/LoginTest.spec.ts
+++ src/tests/LoginTest.spec.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "@playwright/test";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
+import { decrypt } from "../utils/CryptojsUtil";
 
 test("salesforce login test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.navigateToLoginPage();
-  await loginPage.fillUsername("krthiksantosh@gmail.dev");
-  await loginPage.fillPassword("Salesforce2025");
+  await loginPage.fillUsername(process.env.userid!);
+  await loginPage.fillPassword(decrypt(process.env.password!));
 
   const homePage = await loginPage.clickLoginButton();
   await homePage.expectServiceTitleToBeVisible();
