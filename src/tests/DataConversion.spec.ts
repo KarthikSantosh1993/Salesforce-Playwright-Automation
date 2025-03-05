0b6a5bcb7322788b6b0718d1b305ee69c184d4cd
karthik <krthiksantosh@gmail.com>
2025-03-05 21:17:31 +0530

    removed data conversion test and added test data for contact creation

--- src/tests/DataConversion.spec.ts
+++ src/tests/DataConversion.spec.ts
@@ -2,15 +2,15 @@ import { test } from "@playwright/test";
 import { convertCsvFileToJsonFile } from "../utils/CSVtoJSONUtil";
 import { exportToCsv, exportToJson, generateTestData } from "../utils/FakerDataUtil";
 
-test("csv to json conversion", async ({ page }) => {
-    convertCsvFileToJsonFile("data.csv", "data.json");
-});
+// test("csv to json conversion", async ({ page }) => {
+//     convertCsvFileToJsonFile("data.csv", "data.json");
+// });
 
-test.only("generate random dynamic data", async ({ page }) => {
-    const testData = generateTestData(20);
-    // Export data to JSON file
-    exportToJson(testData, "testData_generated.json");
-    // Export data to CSV file
-    exportToCsv(testData, "testData_generated.csv");
-});
+// test("generate random dynamic data", async ({ page }) => {
+//     const testData = generateTestData(20);
+//     // Export data to JSON file
+//     exportToJson(testData, "testData_generated.json");
+//     // Export data to CSV file
+//     exportToCsv(testData, "testData_generated.csv");
+// });
 
